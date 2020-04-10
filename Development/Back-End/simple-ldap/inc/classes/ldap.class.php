2814a781f477577fc2152a221cecfc265f287422
krayechi619 <32582262+krayechi619@users.noreply.github.com>
2020-04-10 16:19:50 -0600

    ldap module

---
+++ Development/Back-End/simple-ldap/inc/classes/ldap.class.php
@@ -0,0 +1,47 @@
+<?php
+
+/*	
+ *	Created by Web Services (jenkshaw) 09 Dec 2013
+ *		-- creates a basic LDAP connection to the U of R Pool 
+ *		-- Authentication based on tree 'all.uofr'
+ *		-- Construct 
+ *			- $server	- (optional) requires full server path, including protocol (ldaps://)
+ *			- $port		- (optional) default of 636 set in object
+ *	
+ */	
+
+class ldapUR {
+	
+	private $conn;
+
+	public function __construct($server='ldaps://ldappool.cc.uregina.ca', $port=636) {
+		
+		if ($server == '' || !is_numeric($port)) return false;
+		
+		$this->conn = ldap_connect($server, $port);
+		ldap_set_option($this->conn, LDAP_OPT_PROTOCOL_VERSION, 3);
+		ldap_set_option($this->conn, LDAP_OPT_REFERRALS, 0);
+		ldap_set_option($this->conn, LDAP_OPT_TIMELIMIT, 10);
+	}
+	
+	public function authUser ($username, $password) {
+		
+		if ($username == '' || $password == '') return false;
+		
+		$username = "cn=$username,OU=all,O=uofr";
+		$reply = ldap_bind($this->conn, $username, $password);
+		return ($reply) ? true : false;
+	}
+	
+	public function error () {
+		ldap_get_option($this->conn, LDAP_OPT_ERROR_STRING, $error);
+		return $error;
+	}
+	
+	public function __destruct() {
+		ldap_unbind($this->conn);
+	}
+	
+}
+
+?>
